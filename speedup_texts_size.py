3d966c780f12910ce33b4ea928e3d9b8e5aae3fc
Chiara Polenzani <chiara.polenzani@edu.unifi.it>
2025-06-10 22:30:09 +0200

    Update speedup_texts_size.py

--- speedup_texts_size.py
+++ speedup_texts_size.py
@@ -2,14 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Carica il CSV
-#df = pd.read_csv("build/speedup_variable_size.csv")
-folder_size = [100, 200, 300, 400, 500, 600]
-speedup = [3.04, 3.14, 3.12, 2.93, 2.80, 2.98]
+df = pd.read_csv("build/speedup_variable_size.csv")
 
 # Plot
 plt.figure(figsize=(10, 6))
-plt.plot(folder_size, speedup, marker='o', linestyle='-', color='b', label="Speedup")
-#plt.plot(df["Text Size(MB)"], df["Speedup"], marker='o', linestyle='-', color='red')
+plt.plot(df["Text Size(MB)"], df["Speedup"], marker='o', linestyle='-', color='red')
 plt.title("Speedup trend with respect of the size of the texts set")
 plt.xlabel("Texts set size (MB)")
 plt.ylabel("Speedup")
